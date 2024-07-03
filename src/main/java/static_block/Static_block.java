9f15dfc74b924ce0ba8f2d2c48d481970c1995b2
offic <offic@DESKTOP-GLF4UNU>
2024-07-03 23:22:29 +0600

    static block review.

---
+++ src/main/java/static_block/Static_block.java
@@ -0,0 +1,18 @@
+package static_block;
+
+public class Static_block {
+
+    //it is static block
+    //it is single static block, you could use multiple static block. multiple that means one or more.
+    static {
+        System.out.println("static block");
+    }
+    
+    //-----------------------------------------------
+    //-----------------------------------------------
+    public static void main(String[] args){
+        
+        System.out.println("Main method.");
+    }
+    
+}
