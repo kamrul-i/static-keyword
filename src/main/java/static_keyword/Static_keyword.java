f035180187e477e36bd9d3a5ca1b2c3a696fe644
offic <offic@DESKTOP-GLF4UNU>
2024-07-03 20:00:59 +0600

    Introduction to static keyword.

---
+++ src/main/java/static_keyword/Static_keyword.java
@@ -0,0 +1,8 @@
+package static_keyword;
+
+public class Static_keyword {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome! Introduction to \"static keyword.\"");
+    }
+}
