1e6b51d18d044301d7aad752ed79c6134fdbd0ab
offic <offic@DESKTOP-GLF4UNU>
2024-07-03 20:11:42 +0600

    Types of variable

---
+++ src/main/java/static_keyword/Student.java
@@ -0,0 +1,24 @@
+package static_keyword;
+
+public class Student {
+        /*
+    There are 3 types of variable
+        local variable, 
+        instance variable, 
+        static/class variable
+    
+    1. what is local variable ?
+        A variable that is declared inside the method is called variable.
+        যে variable কোন ১টি method এর মধ্যে declare করা হয় তাকে local variable বলে ।
+        আবার,
+        যে variable কোন ১টি method/constructor/block এর মধ্যে declare করা হয় তাকে local variable বলে ।
+    
+    2. what is instance variable ?
+        A variable that is declared inside the class but outside any method that is called instance variable.
+        যে variable কোন ১টি class এর মধ্যে, তবে যেকোন method এর বাইরে declare করা হয় তাকে instance variable বলে ।
+        instance variable কে কখনো static হিসেবে declare করা হয় না ।
+    
+    3. what is static variable ?
+        যে variable এর সামনে static keyword থাকবে, তাকে static variable বলে ।
+     */
+}
